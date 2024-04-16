4009202a92a9e012f14af00cf484e8705ff48ded
Renatarafaelaalves <renatarafaela85@yahoo.com.br>
2024-04-16 14:27:07 -0300

    criacao do endpoint login

--- src/roteadores/rotasUsuario.js
+++ src/roteadores/rotasUsuario.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const cadastrarUsuario = require("../controladores/usuarios/cadastroUsuario");
+const loginDeUsuarios = require("../controladores/usuarios/loginUsuario");
+const autenticandoRotas = require("../intermediarios/autenticacao");
 
 const rotasUsuario = express.Router();
 
 rotasUsuario.post("/usuario", cadastrarUsuario);
+rotasUsuario.post("/login", loginDeUsuarios)
+
+rotasUsuario.use(autenticandoRotas)
+
 
 
 module.exports = rotasUsuario;
